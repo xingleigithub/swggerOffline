df3ff71b10f4dab8cd8eff60f85fa7366c8b83b1
xingleigithua <859853145@qq.com>
2019-03-18 10:45:17 +0800

    生成离线的pdf和html

---
+++ src/main/java/com/swagger/offline/App.java
@@ -0,0 +1,20 @@
+package com.swagger.offline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Title: 启动类
+ * @ClassName: App.java
+ * @Description:
+ * @Copyright 2杭州凯立通信
+ * @version V1.0
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ src/main/java/com/swagger/offline/config/SwaggerConfiguration.java
@@ -0,0 +1,53 @@
+package com.swagger.offline.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**
+ * @version V1.0
+ * @Title: Swagger配置类
+ * @author: 邢磊
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket buildDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInfo())
+                .select()
+                //要扫描的API(Controller)基础包
+                .apis(RequestHandlerSelectors.basePackage("com.swagger.offline.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * @param
+     * @return springfox.documentation.service.ApiInfo
+     * @Title: 构建API基本信息
+     * @methodName: buildApiInfo
+     * @Description:
+     * @author: 邢磊
+     * @date: 2017-12-11  8:44
+     */
+    private ApiInfo buildApiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("用户信息API文档")
+                .description("这里除了查看接口功能外，还提供了调试测试功能")
+                .contact("xinglei")
+                .version("1.0")
+                .build();
+
+    }
+}
